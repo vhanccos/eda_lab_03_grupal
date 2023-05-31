6359db78bbb0ff246a5bc5f66f79d87d653734a4
JeanChara <jcharaco@unsa.edu.pe>
2023-05-31 16:54:51 -0500

    añadido atributos y constructor para clase Persona

--- Ejercicio 2/Persona.java
+++ Ejercicio 2/Persona.java
@@ -1,3 +1,15 @@
 public class Persona {
+	
+	private int edad;
+	private String nombre;
+	
+	public Persona() {
+		this.edad=0;
+		this.nombre = "Desconocido";
+	}
+	public Persona(int edad, String nombre) {
+		this.edad = edad;
+		this.nombre = nombre;
+	}
 
 }
