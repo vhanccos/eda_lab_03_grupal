9b8d87baaaa1a8941b066e926251f43da4b76d2d
carrascoArles <acarrascoch@unsa.edu.pe>
2023-06-02 15:00:44 -0500

    se termino el test

--- Ejercicio03/TestCola.java
+++ Ejercicio03/TestCola.java
@@ -9,6 +9,30 @@ public class TestCola {
         cola.añadirCola(10);
         cola.añadirCola(40);
         cola.añadirCola(90);
+        System.out.println("Tamaño de la cola: " + cola.tamaño());
         System.out.println("El primer nodo es : " + cola.obtenerCabeza());
+        System.out.println("El ultimo nodo es : " + cola.obtenerUltimo());
+        System.out.println(cola);
+
+        System.out.println("Elemento desencolado: " + cola.eliminarCola());
+        System.out.println(cola);
+        System.out.println("El primer nodo es : " + cola.obtenerCabeza());
+        
+        System.out.println("\nSe añadio un nodo 15");
+        
+        cola.añadirCola(15);
+        System.out.println(cola);
+        System.out.println("El ultimo nodo es : " + cola.obtenerUltimo());
+        
+        System.out.println("Se elimina toda la cola");
+
+        while (!cola.vacia()) {
+            System.out.println("Elemento desencolado: " + cola.eliminarCola());
+        }
+        try {
+            cola.eliminarCola();
+        } catch (Exception e) {
+            System.out.println("Excepción al desencolar: " + e.getMessage());
+        }
     }
 }
