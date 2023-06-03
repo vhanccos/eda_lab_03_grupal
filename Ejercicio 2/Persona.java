f1ea92929fddfa9f4aeeaee7ee9366f746716235
Jean <jcharaco@unsa.edu.pe>
2023-06-02 21:22:12 -0500

    añadido metodo toString, set y get para atributos en clase persona

--- Ejercicio 2/Persona.java
+++ Ejercicio 2/Persona.java
@@ -11,5 +11,20 @@ public class Persona {
 		this.edad = edad;
 		this.nombre = nombre;
 	}
+	public void setNombre(String nombre) {
+		this.nombre =  nombre;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public String toString() {
+		return "[Nombre: "+nombre+", Edad: "+edad+"]";
+	}
 
 }
