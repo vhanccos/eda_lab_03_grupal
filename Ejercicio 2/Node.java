e92f0c7a743f2ee97d2ccc18103a503bedb9387b
Jean <jcharaco@unsa.edu.pe>
2023-06-02 20:55:35 -0500

    Creacion de clase Node y Stack(Pila)

---
+++ Ejercicio 2/Node.java
@@ -0,0 +1,4 @@
+public class Node<T> {
+	private T dato;
+	private Node<T> nextNode;
+}
---
+++ Ejercicio 2/Stack.java
@@ -0,0 +1,15 @@
+public class Stack<T> {
+	
+	private Node<T> root;
+	
+	public void push(T x) {
+		
+	}
+	public T pop() {
+	}
+	public T top() {
+	}
+	public boolean isEmpty() {
+		
+	}
+}
