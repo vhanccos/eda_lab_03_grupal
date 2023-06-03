e70faba05dbcfcf294a8b3fd2be2fbb6649f6633
Jean <jcharaco@unsa.edu.pe>
2023-06-02 21:04:13 -0500

    añadido constructores para clase Node

--- Ejercicio 2/Node.java
+++ Ejercicio 2/Node.java
@@ -1,4 +1,13 @@
 public class Node<T> {
 	private T dato;
 	private Node<T> nextNode;
+
+	public Node(T dato, Node<T> nextNode){
+		this.dato = dato;
+		this.nextNode = nextNode;
+	}
+	public Node(T dato){
+		this.dato = dato;
+		this.nextNode = null;
+	}
 }
